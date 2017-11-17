c17a9f128f6338186a4ed2f51861f6ee37a5b62d
bodiab19 <bogdanbalias19@gmail.com>
2017-11-17 23:10:17 +0200

    Прив`язка даних

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+const todos = [
+    {
+        title: 'learn js',
+        completed: true
+    },
+    {
+        title: 'Learn angular',
+        completed: false
+    },
+    {
+        title: 'Write app',
+        completed: false
+    }
+    
+];
+
 @Component({
     moduleId: module.id,
     selector: 'app',
@@ -9,4 +25,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent{
     title = 'Angular 2Do';
+    todos = todos; 
+    
+    toggle(todo: any) {
+    todo.completed = !todo.completed;
+    }
+    
+    delete(todo: any){
+    let index = this.todos.indexOf(todo);
+    
+    if(index > -1){
+    this.todos.splice(index, 1);
+    }
+    }
 }

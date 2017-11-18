2b40641584363d6461861d38c80b71a92c8f52f1
bodiab19 <bogdanbalias19@gmail.com>
2017-11-18 23:34:08 +0200

    Interaction of components

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Todo } from './shared/todo';
+import { todos } from './shared/data';
+
+
 @Component({
     moduleId: module.id,
     selector: 'app',
@@ -9,4 +13,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent{
     title: string = 'Angular 2Do';
+    todos: Todo[] = todos;
+    
+    create(title: string){
+        const todo = new Todo(title);
+        this.todos.push(todo);
+    }
 }
